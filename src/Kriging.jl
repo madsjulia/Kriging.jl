ab8d5e4cfd7d4d5823f2c9b27aad8bec69c6b1e7
Daniel O'Malley <omalled@lanl.gov>
2016-10-07 18:29:39 -0600

    add conditional simulation

--- src/Kriging.jl
+++ src/Kriging.jl
@@ -1,11 +1,40 @@
 module Kriging
 
-gaussiancov(h, maxcov, scale) = maxcov * exp(-(h * h) / (scale * scale))
-expcov(h, maxcov, scale) = maxcov * exp(-h / scale)
-sphericalcov(h, maxcov, scale) = (h <= scale ? maxcov * (1 - 1.5 * h / (scale) + .5 * (h / scale) ^ 3) : 0.)
+import NearestNeighbors
+import Permutations
+
+gaussiancov(h, maxcov, scale, nugget=0.) = maxcov * exp(-(h * h) / (scale * scale)) + (h > 0 ? 0. : nugget)
+expcov(h, maxcov, scale, nugget=0.) = maxcov * exp(-h / scale) + (h > 0 ? 0. : nugget)
+sphericalcov(h, maxcov, scale, nugget=0.) = (h <= scale ? maxcov * (1 - 1.5 * h / (scale) + .5 * (h / scale) ^ 3) : 0.) + (h > 0 ? 0. : nugget)
+
+function simplekrige(mu, x0mat::Matrix, X::Matrix, Z::Vector, cov)
+	result = fill(mu, size(x0mat, 2))
+	resultvariance = fill(cov(0), size(x0mat, 2))
+	covmat = getcovmat(X, cov)
+	pinvcovmat = pinv(covmat)
+	covvec = Array(Float64, size(X, 2))
+	x = Array(Float64, size(X, 2))
+	for i = 1:size(x0mat, 2)
+		getcovvec!(covvec, x0mat[:, i], X, cov)
+		A_mul_B!(x, pinvcovmat, covvec)
+		for j = 1:size(X, 2)
+			result[i] += (Z[j] - mu) * x[j]
+			resultvariance[i] -= covvec[j] * x[j]
+		end
+	end
+	return result, resultvariance
+end
 
 function krige(x0mat::Matrix, X::Matrix, Z::Vector, cov)
+	return krigenew(x0mat, X, Z, cov)[1]
+end
+
+function krigenew(x0mat::Matrix, X::Matrix, Z::Vector, cov)
+	if size(X, 2) != length(Z)
+		error("number of points and observations don't match")
+	end
 	result = zeros(size(x0mat, 2))
+	resultvariance = fill(cov(0), size(x0mat, 2))
 	covmat = getcovmat(X, cov)
 	bigmat = [covmat ones(size(X, 2)); ones(size(X, 2))' 0.]
 	ws = Array(Float64, size(x0mat, 2))
@@ -21,8 +50,46 @@ function krige(x0mat::Matrix, X::Matrix, Z::Vector, cov)
 		for j = 1:size(X, 2)
 			result[i] += Z[j] * x[j]
 		end
+		for j = 1:length(bigvec)
+			resultvariance[i] -= bigvec[j] * x[j]
+		end
 	end
-	return result
+	return result, resultvariance
+end
+
+function condsim(x0mat::Matrix, X::Matrix, Z::Vector, cov, numneighbors, numobsneighbors=length(Z); neighborsearch=1000)
+	kdtree = NearestNeighbors.KDTree(x0mat)
+	nnindices, _ = NearestNeighbors.knn(kdtree, x0mat, neighborsearch, true)
+	obs_kdtree = NearestNeighbors.KDTree(X)
+	nnindices_obs, _ = NearestNeighbors.knn(obs_kdtree, x0mat, numobsneighbors, true)
+	z0 = Array(Float64, size(x0mat, 2))
+	filledin = fill(false, size(x0mat, 2))
+	perm = Permutations.RandomPermutation(size(x0mat, 2))
+	maxvar = 0.
+	for i = 1:size(x0mat, 2)
+		thisx0 = reshape(x0mat[:, perm[i]], 3, 1)
+		neighbors = nnindices[perm[i]]
+		obs_neighbors = nnindices_obs[perm[i]]
+		numfilled = sum(filledin[neighbors])
+		bigX = Array(Float64, 3, min(numneighbors, numfilled) + numobsneighbors)
+		bigZ = Array(Float64, min(numneighbors, numfilled) + numobsneighbors)
+		bigX[:, 1:numobsneighbors] = X[:, obs_neighbors]
+		bigZ[1:numobsneighbors] = Z[obs_neighbors]
+		bigXcount = numobsneighbors + 1
+		j = 1
+		while j <= length(neighbors) && bigXcount <= size(bigX, 2)
+			if filledin[neighbors[j]]
+				bigX[:, bigXcount] = x0mat[:, neighbors[j]]
+				bigZ[bigXcount] = z0[neighbors[j]]
+				bigXcount += 1
+			end
+			j += 1
+		end
+		mu, var = krigenew(thisx0, bigX, bigZ, cov)
+		z0[perm[i]] = mu[1] + sqrt(max(0., var[1])) * randn()
+		filledin[perm[i]] = true
+	end
+	return z0
 end
 
 function getcovmat(X::Matrix, cov::Function)
@@ -59,6 +126,35 @@ end
 
 function estimationerror(w::Vector, x0::Vector, X::Matrix, covmat::Matrix, covvec::Vector, cov0::Number)
 	return cov0 + dot(w, covmat * w) - 2 * dot(w, covvec)
+end
+
+function getgridpoints(xs, ys, zs)
+	gridxyz = Array(Float64, 3, length(xs) * length(ys) * length(zs))
+	i = 1
+	for x in xs
+		for y in ys
+			for z in zs
+				gridxyz[1, i] = x
+				gridxyz[2, i] = y
+				gridxyz[3, i] = z
+				i += 1
+			end
+		end
+	end
+	return gridxyz
+end
+
+function grid2layers(obs, xs, ys, zs)
+	layers = Array(Array{Float64, 2}, length(zs))
+	for k = 1:length(zs)
+		layers[k] = Array(Float64, length(xs), length(ys))
+		for i = 1:length(xs)
+			for j = 1:length(ys)
+				layers[k][i, j] = obs[k + (j - 1) * length(zs) + (i - 1) * length(zs) * length(ys)]
+			end
+		end
+	end
+	return layers
 end
 
 end
