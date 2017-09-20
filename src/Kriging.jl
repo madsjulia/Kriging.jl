880e2c9f64d06b46e2610f92d585892130f8dba2
Daniel O'Malley <omalled@lanl.gov>
2017-09-20 15:13:28 -0600

    Array(...)->Array{...}(...)

--- src/Kriging.jl
+++ src/Kriging.jl
@@ -15,8 +15,8 @@ function distsquared(a, b)
 end
 
 function inversedistance(x0mat::Matrix, X::Matrix, Z::Vector, pow)
-	result = Array(Float64, size(x0mat, 2))
-	weights = Array(Float64, size(X, 2))
+	result = Array{Float64}(size(x0mat, 2))
+	weights = Array{Float64}(size(X, 2))
 	for i = 1:size(x0mat, 2)
 		for j = 1:size(X, 2)
 			weights[j] = 1 / distsquared(x0mat[:, i], X[:, j]) ^ (.5 * pow)
@@ -31,8 +31,8 @@ function simplekrige(mu, x0mat::Matrix, X::Matrix, Z::Vector, cov)
 	resultvariance = fill(cov(0), size(x0mat, 2))
 	covmat = getcovmat(X, cov)
 	pinvcovmat = pinv(covmat)
-	covvec = Array(Float64, size(X, 2))
-	x = Array(Float64, size(X, 2))
+	covvec = Array{Float64}(size(X, 2))
+	x = Array{Float64}(size(X, 2))
 	for i = 1:size(x0mat, 2)
 		getcovvec!(covvec, x0mat[:, i], X, cov)
 		A_mul_B!(x, pinvcovmat, covvec)
@@ -56,12 +56,12 @@ function krigenew(x0mat::Matrix, X::Matrix, Z::Vector, cov)
 	resultvariance = fill(cov(0), size(x0mat, 2))
 	covmat = getcovmat(X, cov)
 	bigmat = [covmat ones(size(X, 2)); ones(size(X, 2))' 0.]
-	ws = Array(Float64, size(x0mat, 2))
-	bigvec = Array(Float64, size(X, 2) + 1)
+	ws = Array{Float64}(size(x0mat, 2))
+	bigvec = Array{Float64}(size(X, 2) + 1)
 	bigvec[end] = 1
 	bigmatpinv = pinv(bigmat)
-	covvec = Array(Float64, size(X, 2))
-	x = Array(Float64, size(X, 2) + 1)
+	covvec = Array{Float64}(size(X, 2))
+	x = Array{Float64}(size(X, 2) + 1)
 	for i = 1:size(x0mat, 2)
 		bigvec[1:end-1] = getcovvec!(covvec, x0mat[:, i], X, cov)
 		bigvec[end] = 1
@@ -81,7 +81,7 @@ function condsim(x0mat::Matrix, X::Matrix, Z::Vector, cov, numneighbors, numobsn
 	nnindices, _ = NearestNeighbors.knn(kdtree, x0mat, neighborsearch, true)
 	obs_kdtree = NearestNeighbors.KDTree(X)
 	nnindices_obs, _ = NearestNeighbors.knn(obs_kdtree, x0mat, numobsneighbors, true)
-	z0 = Array(Float64, size(x0mat, 2))
+	z0 = Array{Float64}(size(x0mat, 2))
 	filledin = fill(false, size(x0mat, 2))
 	perm = randperm(size(x0mat, 2))
 	maxvar = 0.
@@ -90,8 +90,8 @@ function condsim(x0mat::Matrix, X::Matrix, Z::Vector, cov, numneighbors, numobsn
 		neighbors = nnindices[perm[i]]
 		obs_neighbors = nnindices_obs[perm[i]]
 		numfilled = sum(filledin[neighbors])
-		bigX = Array(Float64, size(x0mat, 1), min(numneighbors, numfilled) + numobsneighbors)
-		bigZ = Array(Float64, min(numneighbors, numfilled) + numobsneighbors)
+		bigX = Array{Float64}(size(x0mat, 1), min(numneighbors, numfilled) + numobsneighbors)
+		bigZ = Array{Float64}(min(numneighbors, numfilled) + numobsneighbors)
 		bigX[:, 1:numobsneighbors] = X[:, obs_neighbors]
 		bigZ[1:numobsneighbors] = Z[obs_neighbors]
 		bigXcount = numobsneighbors + 1
@@ -112,7 +112,7 @@ function condsim(x0mat::Matrix, X::Matrix, Z::Vector, cov, numneighbors, numobsn
 end
 
 function getcovmat(X::Matrix, cov::Function)
-	covmat = Array(Float64, (size(X, 2), size(X, 2)))
+	covmat = Array{Float64}((size(X, 2), size(X, 2)))
 	cov0 = cov(0)
 	for i = 1:size(X, 2)
 		covmat[i, i] = cov0
@@ -147,8 +147,21 @@ function estimationerror(w::Vector, x0::Vector, X::Matrix, covmat::Matrix, covve
 	return cov0 + dot(w, covmat * w) - 2 * dot(w, covvec)
 end
 
+function getgridpoints(xs, ys)
+	gridxyz = Array{Float64}(2, length(xs) * length(ys))
+	i = 1
+	for x in xs
+		for y in ys
+			gridxyz[1, i] = x
+			gridxyz[2, i] = y
+			i += 1
+		end
+	end
+	return gridxyz
+end
+
 function getgridpoints(xs, ys, zs)
-	gridxyz = Array(Float64, 3, length(xs) * length(ys) * length(zs))
+	gridxyz = Array{Float64}(3, length(xs) * length(ys) * length(zs))
 	i = 1
 	for x in xs
 		for y in ys
@@ -164,9 +177,9 @@ function getgridpoints(xs, ys, zs)
 end
 
 function grid2layers(obs, xs, ys, zs)
-	layers = Array(Array{Float64, 2}, length(zs))
+	layers = Array{Array{Float64, 2}}(length(zs))
 	for k = 1:length(zs)
-		layers[k] = Array(Float64, length(xs), length(ys))
+		layers[k] = Array{Float64}(length(xs), length(ys))
 		for i = 1:length(xs)
 			for j = 1:length(ys)
 				layers[k][i, j] = obs[k + (j - 1) * length(zs) + (i - 1) * length(zs) * length(ys)]
